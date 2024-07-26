47c3af149e09889c979b89810627e402245872c1
Tom <anderatt@gmail.com>
2024-07-26 10:31:11 -0300

    Flask configuração inicial

---
+++ flaskservice.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from flask import Flask # type: ignore
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
