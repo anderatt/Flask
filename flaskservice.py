6a3b48d9e6515d6c3cb5f31f383d5737dc8c707d
Tom <anderatt@gmail.com>
2024-07-26 11:15:45 -0300

    Teste New DB

--- flaskservice.py
+++ flaskservice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask # type: ignorecccc
+from flask import Flask # type: ignore
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
@@ -8,5 +8,12 @@ app.config['JSON_SORT_KEYS'] = False
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db')
+def db_access():
+    return 'Acessando o Database!'
+
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
